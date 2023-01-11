055eb5bd7e5eaccb4ba2ba916c1cfe201b80e1ec
dhjoe <ssd351@daum.net>
2023-01-11 12:16:36 +0900

    step04activitymove 모듈에 국가 화면에서 메인으로 이동하는 버튼 추가

--- step04activitymove/src/main/java/com/example/step04activitymove/GermanActivity.java
+++ step04activitymove/src/main/java/com/example/step04activitymove/GermanActivity.java
@@ -1,6 +1,7 @@
 package com.example.step04activitymove;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,21 @@ public class GermanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_german);
+
+        /*
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        };
+        */
+
+        findViewById(R.id.finishBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
